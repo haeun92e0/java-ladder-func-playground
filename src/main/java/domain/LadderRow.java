6fab58a0cecadf405b0d596a60ece0d0064d3ed9
haeun92e0 <gracekim8729@gmail.com>
2026-04-30 21:25:36 +0900

    Refactor: 세로줄 개수를 나타내는 인자 네이밍 수정(count -> width)

--- src/main/java/domain/LadderRow.java
+++ src/main/java/domain/LadderRow.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class LadderRow {
     private final List<LadderStep> points;
 
-    public LadderRow(int count, LadderRowStrategy strategy) {
-        this.points = strategy.generate(count);
-    }// count수만큼 가로줄 생성
+    public LadderRow(int width, LadderRowStrategy strategy) {
+        this.points = strategy.generate(width);
+    }// width수만큼 가로줄 생성
 
     public int move(int index) {
         return points.get(index).positionAfterMoving();
